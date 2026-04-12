e895447481db6c3e706b8f2ca232bf7d7d3c5507
Navid Ghanian <navidghanian@mac.home>
2026-04-12 13:45:25 +0200

    Small change in S3 bucket names

--- cdk/lib/cdk-stack-v2.ts
+++ cdk/lib/cdk-stack-v2.ts
@@ -149,8 +149,8 @@ export class CdkStackV2 extends cdk.Stack {
     // ------------------------------------------------------------
     // Outbound bucket. This bucket is used by the pdf transformation service
     // to deliver files to Paligo - aka outbound files.
-    const outboundBucket = ssm.StringParameter.valueForStringParameter(this, '/env/s3/microservice-v2/outbound');
-    const inboundBucket = ssm.StringParameter.valueForStringParameter(this, '/env/s3/microservice-v2/inbound');
+    const outboundBucket = ssm.StringParameter.valueForStringParameter(this, '/env/s3/microservice/outbound-v2');
+    const inboundBucket = ssm.StringParameter.valueForStringParameter(this, '/env/s3/microservice/inbound-v2');
 
     // Import s3 bucket
     let s3OutboundBucket = Bucket.fromBucketArn(this, 'outboundBucket', `arn:aws:s3:::${outboundBucket}`)
